4c5f598f65304987e30bfed9815d5a10cff5f620
alexis12vlo <84141707+alexis12vlo@users.noreply.github.com>
2021-08-11 17:34:36 -0500

    AA

--- final.js
+++ final.js
@@ -15,12 +15,40 @@ let h5 = document.querySelector('div.container3 > div.container3con > #con3 > h5
 let h52 = document.querySelector('div.container3 > div.container3con > #con32 > h5');
 let h53 = document.querySelector('div.container3 > div.container3con > #con33 > h5');
 
-
+const rojo=document.querySelector('#rojo');
 const con3=document.querySelector('#con3');
 const con32=document.querySelector('#con32');
 const con33=document.querySelector('#con33');  
 const con4 = document.querySelector('.container4');
 const imagenes4 = document.querySelector('#imag4 , .container4conimagenes');
+const blanco1 = document.querySelector('#blanco1');
+const blanco2 = document.querySelector('#blanco2');
+const blanco3 = document.querySelector('#blanco3');
+const blanco4 = document.querySelector('#blanco4');
+const blanco5 = document.querySelector('#blanco5');
+const blanco6 = document.querySelector('#blanco6');
+const blanco7 = document.querySelector('#blanco7');
+const blanco8 = document.querySelector('#blanco8');
+const letra1= document.querySelector('#letra1');
+const letra2= document.querySelector('#letra2');
+const letra3= document.querySelector('#letra3');
+const letra4= document.querySelector('#letra4');
+const letra5= document.querySelector('#letra5');
+const letra6= document.querySelector('#letra6');
+const letra7= document.querySelector('#letra7');
+const letra8= document.querySelector('#letra8');
+const nombre= document.querySelector('#nombre');
+const nombre2= document.querySelector('#nombre2');
+const nombre3= document.querySelector('#nombre3');
+const nombre4= document.querySelector('#nombre4');
+const nombre5= document.querySelector('#nombre5');
+const nombre6= document.querySelector('#nombre6');
+const nombre7= document.querySelector('#nombre7');
+const nombre8= document.querySelector('#nombre8');
+const containersub=document.querySelector('#containersub');
+const letrasub=document.querySelector('#letrasub');
+
+
 
 const con5 = document.querySelector('.container5'); 
 
@@ -48,9 +76,42 @@ iconDark.addEventListener('click', () =>{
    con32.style.backgroundColor = '#161C2D';
    con33.style.backgroundColor = '#161C2D';
   
-   con4.style.backgroundColor = '#161C2D';
-    
-   
+   con4.style.backgroundColor = '#121725';
+    blanco1.style.backgroundColor = '#161C2D';
+    blanco2.style.backgroundColor = '#161C2D';
+    blanco3.style.backgroundColor = '#161C2D';
+    blanco4.style.backgroundColor = '#161C2D';
+    blanco5.style.backgroundColor = '#161C2D';  
+    blanco6.style.backgroundColor = '#161C2D'; 
+    blanco7.style.backgroundColor = '#161C2D'; 
+    blanco8.style.backgroundColor = '#161C2D';
+    letra1.style.backgroundColor = '#161C2D';
+    nombre.style.color = '#FFFFFF';
+    nombre2.style.color = '#FFFFFF';
+    nombre3.style.color = '#FFFFFF';
+    nombre4.style.color = '#FFFFFF';
+    nombre5.style.color = '#FFFFFF';
+    nombre6.style.color = '#FFFFFF';
+    nombre7.style.color = '#FFFFFF';
+    nombre8.style.color = '#FFFFFF';
+    containersub.style.backgroundColor='#161C2D';
+    letrasub.style.color = '#FFFFFF';
+
+
+    letra2.style.backgroundColor = '#161C2D';
+  letra3.style.backgroundColor = '#161C2D'; 
+  letra4.style.backgroundColor = '#161C2D'; 
+  letra5.style.backgroundColor = '#161C2D'; 
+  letra6.style.backgroundColor = '#161C2D';
+letra7.style.backgroundColor = '#161C2D';
+letra8.style.backgroundColor = '#161C2D';
+rojo.style.color = '#F06962';
+
+
+
+
+
+
    con5.style.backgroundColor = '#161C2D';
 
 
@@ -76,7 +137,37 @@ iconDark.addEventListener('click', () =>{
    con33.style.backgroundColor = 'white';
      con4.style.backgroundColor = 'white';
    con5.style.backgroundColor = 'white';
- 
+  con4.style.backgroundColor = '#FFFFFF';
+    blanco1.style.backgroundColor = '#FFFFFF';
+    blanco2.style.backgroundColor = '#FFFFFF';
+    blanco3.style.backgroundColor = '#FFFFFF';
+    blanco4.style.backgroundColor = '#FFFFFF';
+    blanco5.style.backgroundColor = '#FFFFFF';  
+    blanco6.style.backgroundColor = '#FFFFFF'; 
+    blanco7.style.backgroundColor = '#FFFFFF'; 
+    blanco8.style.backgroundColor = '#FFFFFF';
+    letra1.style.backgroundColor = '#FFFFFF';
+    nombre.style.color = '#000000';
+    nombre2.style.color = '#000000';
+    nombre3.style.color = '#000000';
+    nombre4.style.color = '#000000';
+    nombre5.style.color = '#000000';
+    nombre6.style.color = '#000000';
+    nombre7.style.color = '#000000';
+    nombre8.style.color = '#000000';
+    containersub.style.backgroundColor='#161C2D';
+    letrasub.style.color = '#FFFFFF';
+
+
+    letra2.style.backgroundColor = '#FFFFFF';
+  letra3.style.backgroundColor = '#FFFFFF'; 
+  letra4.style.backgroundColor = '#FFFFFF'; 
+  letra5.style.backgroundColor = '#FFFFFF'; 
+  letra6.style.backgroundColor = '#FFFFFF';
+letra7.style.backgroundColor = '#FFFFFF';
+letra8.style.backgroundColor = '#FFFFFF';
+rojo.style.color = '#F06962';
+
   }
 
 })
